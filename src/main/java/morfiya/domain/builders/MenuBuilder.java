5b8479964945dacaa9be6eb07316e41578dee6b9
Unknown <garcia.diego1990@gmail.com>
2017-11-23 22:35:41 -0300

    Armado fake de cliente (Falla: problema en la configuracion)

--- src/main/java/morfiya/domain/builders/MenuBuilder.java
+++ src/main/java/morfiya/domain/builders/MenuBuilder.java
@@ -1,85 +1,85 @@
-//package morfiya.domain.builders;
-//
-//import java.time.LocalDate;
-//
-//import morfiya.domain.Categoria;
-//import morfiya.domain.Menu;
-//import morfiya.utils.DateFormatter;
-//
-//public class MenuBuilder {
-//	private String nombre;
-//	private String descripcion;
-//	private Categoria categoria;
-//	private Double valorDelivery;
-//	private LocalDate fechaVigenciaDesde;
-//	private LocalDate fechaVigenciaHasta;
-//	private Double precio;
-//	private Integer cantidadMinima;
-//	private Integer cantidadMinima2;
-//	private Double precioCantidadMinima;
-//	private Double precioCantidadMinima2;
-//	private Integer cantidadMaxVtasPorDia;
-//
-//	public static MenuBuilder mkBuilder() {
-//		return new MenuBuilder();
-//	}
-//
-//	public MenuBuilder withNombre(String nombre) {
-//		this.nombre = nombre;
-//		return this;
-//	}
-//
-//	public MenuBuilder withDescripcion(String descripcion) {
-//		this.descripcion = descripcion;
-//		return this;
-//	}
-//
-//	public MenuBuilder withCategoria(Categoria categoria) {
-//		this.categoria = categoria;
-//		return this;
-//	}
-//
-//	public MenuBuilder withValorDelivery(Double valor) {
-//		this.valorDelivery = valor;
-//		return this;
-//	}
-//
-//	public MenuBuilder withFechaVigenciaDesdeYHasta(String desde, String hasta) {
-//		this.fechaVigenciaDesde = DateFormatter.formatLocalDate(desde);
-//		this.fechaVigenciaHasta = DateFormatter.formatLocalDate(hasta);
-//		return this;
-//	}
-//
-//	public MenuBuilder withPrecio(Double precio) {
-//		this.precio = precio;
-//		return this;
-//	}
-//
-//	public MenuBuilder withCantidadMinima1y2(Integer cantidad1, Integer cantidad2) {
-//		this.cantidadMinima = cantidad1;
-//		this.cantidadMinima2 = cantidad2;
-//		return this;
-//	}
-//
-//	public MenuBuilder withPrecioCantidadMinima1y2(Double precio1, Double precio2) {
-//		this.precioCantidadMinima = precio1;
-//		this.precioCantidadMinima2 = precio2;
-//		return this;
-//	}
-//
-//	public MenuBuilder withCantidadMaxVtasPorDia(Integer cantidad) {
-//		this.cantidadMaxVtasPorDia = cantidad;
-//		return this;
-//
-//	}
-//
-//	public Menu build() {
-//		Menu menu = new Menu(this.nombre, this.descripcion, this.categoria, this.valorDelivery, this.fechaVigenciaDesde,
-//				this.fechaVigenciaHasta, this.precio, this.cantidadMinima, this.cantidadMinima2,
-//				this.precioCantidadMinima, this.precioCantidadMinima2,
-//				this.cantidadMaxVtasPorDia)
-//				
-//		return menu;
-//	}
-//
-//}
+package morfiya.domain.builders;
+
+import java.time.LocalDate;
+
+import morfiya.domain.Categoria;
+import morfiya.domain.Menu;
+import morfiya.utils.DateFormatter;
+
+public class MenuBuilder {
+	private String nombre;
+	private String descripcion;
+	private Categoria categoria;
+	private Double valorDelivery;
+	private LocalDate fechaVigenciaDesde;
+	private LocalDate fechaVigenciaHasta;
+	private Double precio;
+	private Integer cantidadMinima;
+	private Integer cantidadMinima2;
+	private Double precioCantidadMinima;
+	private Double precioCantidadMinima2;
+	private Integer cantidadMaxVtasPorDia;
+
+	public static MenuBuilder mkBuilder() {
+		return new MenuBuilder();
+	}
+
+	public MenuBuilder withNombre(String nombre) {
+		this.nombre = nombre;
+		return this;
+	}
+
+	public MenuBuilder withDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+		return this;
+	}
+
+	public MenuBuilder withCategoria(Categoria categoria) {
+		this.categoria = categoria;
+		return this;
+	}
+
+	public MenuBuilder withValorDelivery(Double valor) {
+		this.valorDelivery = valor;
+		return this;
+	}
+
+	public MenuBuilder withFechaVigenciaDesdeYHasta(String desde, String hasta) {
+		this.fechaVigenciaDesde = DateFormatter.formatLocalDate(desde);
+		this.fechaVigenciaHasta = DateFormatter.formatLocalDate(hasta);
+		return this;
+	}
+
+	public MenuBuilder withPrecio(Double precio) {
+		this.precio = precio;
+		return this;
+	}
+
+	public MenuBuilder withCantidadMinima1y2(Integer cantidad1, Integer cantidad2) {
+		this.cantidadMinima = cantidad1;
+		this.cantidadMinima2 = cantidad2;
+		return this;
+	}
+
+	public MenuBuilder withPrecioCantidadMinima1y2(Double precio1, Double precio2) {
+		this.precioCantidadMinima = precio1;
+		this.precioCantidadMinima2 = precio2;
+		return this;
+	}
+
+	public MenuBuilder withCantidadMaxVtasPorDia(Integer cantidad) {
+		this.cantidadMaxVtasPorDia = cantidad;
+		return this;
+
+	}
+
+
+	public Menu build() {
+		Menu menu = new Menu(this.nombre, this.descripcion, this.categoria, this.valorDelivery, this.fechaVigenciaDesde.toString(),
+				this.fechaVigenciaHasta.toString(), this.precio, this.cantidadMinima, this.cantidadMinima2,
+				this.precioCantidadMinima, this.precioCantidadMinima2,this.cantidadMaxVtasPorDia);
+				
+		return menu;
+	}
+
+}
--- src/main/java/morfiya/rest/DataFakeRest.java
+++ src/main/java/morfiya/rest/DataFakeRest.java
@@ -1,11 +1,37 @@
 package morfiya.rest;
 
-import morfiya.services.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+import morfiya.domain.Cliente;
+import morfiya.domain.Direccion;
+import morfiya.domain.Localidad;
+import morfiya.domain.LocalizacionMapa;
+import morfiya.domain.builders.ClienteBuilder;
+import morfiya.services.ClienteService;
+
+@Path("/")
 public class DataFakeRest {
-	
-	private MenuService menuService;
-	private ProveedorService proveedorService;
-	private ClienteService clienteService;
 
+	// private MenuService menuService;
+	// private ProveedorService proveedorService;
+	ClienteService service;
+	
+	@GET
+	@Path("/load")
+	@Produces("application/json")
+	public Response load(){
+
+	Direccion direccion = new Direccion(Localidad.ALMIRANTE_BROWN, "12 de octubre", 1234, new LocalizacionMapa(12, 15));
+
+	Cliente cliente1 = new ClienteBuilder().withCuit(123456).withNombreYApellido("Diego", "Maradona").withEmail("d10s@gmail.com")
+			.withTelefono("011", "42100987").withCreditosDisponibles(15.0).withDireccion(direccion).build();
+	
+	
+	service.crearCliente(cliente1);
+	
+	return Response.ok().build();
+	}
 }
