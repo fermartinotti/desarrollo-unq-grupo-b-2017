af5e449ce52619abedbae65e538d68673dc3cfde
Unknown <fermartinotti@gmail.com>
2017-12-17 17:15:56 -0300

    emailsender para proveedor

--- src/main/java/morfiya/domain/EmailSender.java
+++ src/main/java/morfiya/domain/EmailSender.java
@@ -27,4 +27,18 @@ public class EmailSender {
 	return request.getBody();
 	}
 
+	
+	public static com.mashape.unirest.http.JsonNode sendEmail(Proveedor proveedor, String descripcion) throws UnirestException {
+		
+		HttpResponse<com.mashape.unirest.http.JsonNode> request = Unirest.post("https://api.mailgun.net/v3/" + "sandbox1c3b7ddcc6da457d87aa7e485e6feaf9.mailgun.org" + "/messages")
+		    .basicAuth("api", "key-d367f7b5732a3ae710e6e6e823a4fafc")
+		.queryString("from", "postmaster@sandbox1c3b7ddcc6da457d87aa7e485e6feaf9.mailgun.org")
+		.queryString("to", proveedor.getEmail())
+		.queryString("subject", "Gracias por comprar en MorfiYA")
+		.queryString("text", descripcion)
+		.asJson();
+		
+		log.debug("Se envia email con exito con descripcion:" + descripcion);
+		return request.getBody();
+		}
 }
